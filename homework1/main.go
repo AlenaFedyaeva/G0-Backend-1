7301f4ba3944fb72506376bd988068956ba8aaf2
alenafedyaeva <291407@gmail.com>
2021-09-29 11:33:58 +0300

    comment

--- homework1/main.go
+++ homework1/main.go
@@ -17,7 +17,7 @@ func main() {
 			panic("expected http.ResponceWriter to be an http.Flusher")
 		}
 		w.Header().Set("X-Content-Type-Options", "nosniff")
-		for i := 0; i < 10; i++ {
+		for i := 0; i <= 10; i++ {
 			fmt.Fprintf(w, "Chunk # %d \n", i)
 			flusher.Flush()
 			time.Sleep(500 * time.Millisecond)
